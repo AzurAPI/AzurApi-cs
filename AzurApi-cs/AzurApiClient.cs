4cbbbe248d7875fd0d60d69393efbcdc71f45c11
builderb gamer <builderbgamer@gmail.com>
2020-10-12 19:21:15 +0100

    Add project files.

---
+++ AzurApi-cs/AzurApiClient.cs
@@ -0,0 +1,82 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace AzurApi
+{
+    public class AzurApiClient
+    {
+        public AzurApiClient()
+        {
+            Http = new HttpClient();
+        }
+        public readonly HttpClient Http;
+        
+
+
+
+
+        internal async Task<AzurRequest> GetRequest(string url)
+        {
+            HttpResponseMessage Message = null;
+            try
+            {
+                Message = await Http.GetAsync(url);
+                return new AzurRequest
+                {
+                    success = true,
+                    code = 200
+                };
+            }
+            catch (HttpRequestException hex)
+            {
+                return new AzurRequest
+                {
+                    success = false,
+                    code = (int)Message.StatusCode,
+                    error = Message.ReasonPhrase ?? hex.Message
+                };
+            }
+            catch (Exception ex)
+            {
+                return new AzurRequest
+                {
+                    success = false,
+                    code = 0,
+                    error = $"AzurApi-cs lib error, {ex.Message}"
+                };
+            }
+        }
+
+        internal async Task<JObject> GetCustomRequest(string url)
+        {
+            HttpResponseMessage Message = null;
+            try
+            {
+                Message = await Http.GetAsync(url);
+                string Content = await Message.Content.ReadAsStringAsync();
+                return JObject.Parse(Content);
+            }
+            catch (HttpRequestException hex)
+            {
+                return JObject.FromObject(new AzurRequest
+                {
+                    success = false,
+                    code = (int)Message.StatusCode,
+                    error = Message.ReasonPhrase ?? hex.Message
+                });
+            }
+            catch (Exception ex)
+            {
+                return JObject.FromObject(new AzurRequest
+                {
+                    success = false,
+                    code = 0,
+                    error = $"AzurApi-cs lib error, {ex.Message}"
+                });
+            }
+        }
+
+    }
+}
---
+++ AzurApi-cs/AzurRequest.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AzurApi
+{
+    public class AzurRequest
+    {
+        public bool success;
+        public int code;
+        public string error;
+    }
+}
